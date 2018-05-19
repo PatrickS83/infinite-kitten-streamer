5f865d3d44be80e8754c710e3f810ec536b21856
PatrickSchuelke <p-schuelke@gmx.de>
2018-05-19 15:17:38 +0200

    created UI module

---
+++ static/js/UI.js
@@ -0,0 +1,14 @@
+class UI {
+  constructor() {
+    this.elements = {
+      cardContainer: document.querySelector('.cardContainer')
+    };
+    this.init();
+  }
+
+  init() {
+    console.log('it works!');
+  }
+}
+
+export default UI;
