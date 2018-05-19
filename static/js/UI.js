c1c9d009d3a67be721010f30a65b0867a87fbfcb
PatrickSchuelke <p-schuelke@gmx.de>
2018-05-19 15:32:05 +0200

    add functions to create and append cards

--- static/js/UI.js
+++ static/js/UI.js
@@ -7,7 +7,34 @@ class UI {
   }
 
   init() {
-    console.log('it works!');
+    const initialCard = this.createCard();
+    this.appendCard(initialCard);
+  }
+
+  createCard() {
+    const card = document.createElement('div');
+    card.innerHTML = `
+    <div class="col s4">
+      <div class="card">
+          <div class="card-image">
+              <img src="https://materializecss.com/images/sample-1.jpg">
+              <span class="card-title">Card Title</span>
+          </div>
+          <div class="card-content">
+              <p>Enter your theme in the input field!</p>
+          </div>
+          <div class="card-action">
+              <input type="text"></input>
+              <button class="waves-effect waves-light btn">Start Picture Stream!</a>
+          </div>
+      </div>
+    </div>
+    `;
+    return card;
+  }
+
+  appendCard(content, node = this.elements.cardContainer) {
+    node.appendChild(content);
   }
 }
 
