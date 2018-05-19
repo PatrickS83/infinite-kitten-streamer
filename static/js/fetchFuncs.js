4932105571669a31e1274b2b694e9a2feacfbea3
PatrickSchuelke <p-schuelke@gmx.de>
2018-05-19 17:17:10 +0200

    made fetch function importable

---
+++ static/js/fetchFuncs.js
@@ -0,0 +1,15 @@
+export default function flickrTagSearch(tag = 'cats', page = '1') {
+  const apiKey = 'b1a90a68a42acb335863b1fa18f0329e';
+  return fetch('https://api.flickr.com/services/rest/' +
+      '?method=flickr.photos.search' +
+      `&api_key=${apiKey}` +
+      `&page=${page}` +
+      `&tags=${tag}` +
+      '&format=json&nojsoncallback=1')
+    .then(response => response.json())
+    .then(body => body.photos.photo)
+    .then(photos =>
+      photos.map(photo =>
+          `https://farm${photo.farm}.staticflickr.com/` +
+          `${photo.server}/${photo.id}_${photo.secret}_q.jpg`));
+}
