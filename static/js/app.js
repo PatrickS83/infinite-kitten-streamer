4e7c23ed4678faba95eb6978173257e69d7e1845
PatrickSchuelke <p-schuelke@gmx.de>
2018-05-19 18:33:12 +0200

    app can now start multiple streams

--- static/js/app.js
+++ static/js/app.js
@@ -8,17 +8,27 @@ class App {
   }
 
   init() {
-    const initialCard = this.ui.createCard();
-    this.ui.appendCard(initialCard);
+    this.addButtonListener();
   }
 
-  async generate() {
-    const card = new Card();
+  addButtonListener() {
+    const button = document.querySelector('#startButton');
+    const input = document.querySelector('#themeInput');
+    button.addEventListener('click', () => {
+      this.generate(input.value);
+      input.value = '';
+      input.focus();
+    });
+  }
+
+  async generate(theme = 'kittens') {
+    const displayCard = this.ui.createCard(theme);
+    this.ui.appendCard(displayCard);
+    const card = new Card(theme);
     for await (const url of card) {
-      this.ui.changeImg(url);
+      this.ui.changeImg(url, theme);
     }
   }
 }
 
 const app = new App();
-app.generate();
